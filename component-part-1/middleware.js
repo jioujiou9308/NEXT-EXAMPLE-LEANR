8f56573cb9dd34c1be03726537f0657e99db6919
wei.haung <wei.haung@ibuypower.com>
2024-06-13 17:38:18 +0800

    middleware

--- component-part-1/middleware.js
+++ component-part-1/middleware.js
@@ -1,14 +1,26 @@
+// export function middleware(req, res) {
+//   console.log("req.nextUrl.pathname", req.nextUrl.pathname);
+//   console.log("middleware");
+//   if (req.nextUrl.pathname === "/middleware/path-1") {
+//     let cookies = req.cookies.getAll()
+//     console.log('cookies', cookies)
+//   }
+// }
+
+//  * 可以設定多個
+// export const config = {
+//   matcher: ["/middleware/path-1", "/middleware/path-2", "/middleware/path-3"],
+// };
+
+// ------------------------
+import { NextResponse } from "next/server";
 export function middleware(req, res) {
-  console.log("req.nextUrl.pathname", req.nextUrl.pathname);
-  console.log("middleware");
-  if (req.nextUrl.pathname === "/middleware/path-1") {
-    let cookies = req.cookies.getAll()
-    console.log('cookies', cookies)
-  }
+  console.log("req.url", req.url);
+
+  return NextResponse.redirect(new URL("/language", req.url));
 }
 
-
-// 可以設定多個
+// * 設定單層
 export const config = {
-  matcher: ["/middleware/path-1", "/middleware/path-2", "/middleware/path-3"],
+  matcher: "/middleware/path-2",
 };
